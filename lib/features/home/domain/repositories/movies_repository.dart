5e3073dd97c0eddd0920bdd8cbd3c8bc22b80234
Jonatas Brito <jonatas.furtuna@gmail.com>
2021-11-08 00:29:42 -0300

    injection container created with get_it for application-wide dependency injection

--- lib/features/home/domain/repositories/movies_repository.dart
+++ lib/features/home/domain/repositories/movies_repository.dart
@@ -6,6 +6,8 @@ import '../entities/movie.dart';
 typedef FutureGetMovies = Future<Either<Failure, List<Movie>>>;
 
 abstract class MoviesRepository {
+  MoviesRepository(Object object);
+
   FutureGetMovies getMoviesPopular(String key);
   FutureGetMovies getMoviesInTheaters(String key);
 }
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,5 +1,43 @@
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
+import 'core/network/network_info.dart';
+import 'features/home/data/datasources/movies_remote_data_source.dart';
+import 'features/home/data/repositories/movies_repository.dart';
+import 'features/home/domain/repositories/movies_repository.dart';
+import 'features/home/domain/usecases/get_movies_in_theaters.dart';
+import 'features/home/domain/usecases/get_popular_movies.dart';
+import 'features/home/presentation/cubit/movie_cubit.dart';
+
+// sl == Service Locator
 final sl = GetIt.instance;
 
-void init() {}
+Future<void> init() async {
+  //! Features - Number Movie
+  // Bloc
+  sl.registerFactory(() => MoviePopularCubit(getPopularMovies: sl()));
+
+  // User cases
+  sl.registerSingleton(() => GetPopularMovies(sl()));
+  sl.registerSingleton(() => GetMoviesInTeathers(sl()));
+
+  // Repository
+  sl.registerLazySingleton<MoviesRepository>(
+    () => MoviesRepositoryImpl(
+      networkInfo: sl(),
+      remoteDataSource: sl(),
+    ),
+  );
+
+  // Data sources
+  sl.registerLazySingleton<MoviesRemoteDataSource>(
+      () => MoviesRemoteDataSourceImpl(client: sl()));
+
+  // Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  // External
+  sl.registerLazySingleton(() => http.Client);
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
