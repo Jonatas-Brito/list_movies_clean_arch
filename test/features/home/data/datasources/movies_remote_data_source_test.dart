c04c493e21ee83320ec811c9f6f369d321fab16d
Jonatas Brito <jonatas.furtuna@gmail.com>
2021-11-16 22:19:15 -0300

    tests implemented for remote data source [GetCastPeople] method

--- test/features/home/data/datasources/movies_remote_data_source_test.dart
+++ test/features/home/data/datasources/movies_remote_data_source_test.dart
@@ -7,6 +7,8 @@ import 'package:movies_list/core/key/tmdb_key.dart';
 import 'package:movies_list/features/home/data/datasources/movies_remote_data_source.dart';
 import 'package:http/http.dart' as http;
 import 'package:movies_list/features/home/data/models/movies_model.dart';
+import 'package:movies_list/features/home/data/models/people_credits_models.dart';
+import 'package:movies_list/features/home/domain/entities/cast_people.dart';
 import '../../../../fixtures/fixtures_reader.dart';
 import 'movies_remote_data_source_test.mocks.dart';
 
@@ -25,7 +27,14 @@ void main() {
 
   Map<String, dynamic> tMapTrailers = jsonDecode(fixture('trailer_id.json'));
 
+  Map<String, dynamic> tMapCast = jsonDecode(fixture('cast.json'));
+
   List _dynamicListTrailer = tMapTrailers['results'];
+
+  List _dynamicListCast = tMapCast['cast'];
+
+  CastPeopleModel tMapCastPeople =
+      CastPeopleModel.fromJson(_dynamicListCast[0]);
 
   // print(_dynamicListTrailer[0]['key']);
   String key = ApiKey.key;
@@ -55,6 +64,15 @@ void main() {
             http.Response(fixture('trailer_id.json'), 200));
   }
 
+  void setUpMockHttopClientSuccess200CastList() {
+    tMapMoviesModel.castPeople = [tMapCastPeople];
+
+    String uri =
+        'https://api.themoviedb.org/3/movie/$id/credits?api_key=$key&language=en-US';
+    when(mockHttpClient.get(Uri.parse(uri))).thenAnswer(
+        (realInvocation) async => http.Response(fixture('cast.json'), 200));
+  }
+
   void setUpMockHttopClientFailure404orOther() {
     String uri =
         'https://api.themoviedb.org/3/movie/popular?api_key=$key&language=pt-BR&page=1';
@@ -72,6 +90,13 @@ void main() {
   void setUpMockHttopClientFailure404orOtherYoutubeId() {
     String uri =
         'https://api.themoviedb.org/3/movie/$id/videos?api_key=$key&language=en-US';
+    when(mockHttpClient.get(Uri.parse(uri)))
+        .thenAnswer((_) async => http.Response('Somenthing went wrong', 404));
+  }
+
+  void setUpMockHttopClientFailure404orOtherCastList() {
+    String uri =
+        'https://api.themoviedb.org/3/movie/$id/credits?api_key=$key&language=en-US';
     when(mockHttpClient.get(Uri.parse(uri)))
         .thenAnswer((_) async => http.Response('Somenthing went wrong', 404));
   }
@@ -161,6 +186,15 @@ void main() {
     });
   });
 
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
+
   group('getTrailerId', () {
     test('should must perform a GET request on a URL with "movie.id" and "key"',
         () async {
@@ -193,6 +227,52 @@ void main() {
       setUpMockHttopClientFailure404orOtherYoutubeId();
       // act
       final call = dataSource.getYoutubeId;
+      // assert
+      expect(call(0, key), throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
+
+  group('getListCast', () {
+    test('should must perform a GET request on a URL with "movie.id" and "key"',
+        () async {
+      // arrange
+      setUpMockHttopClientSuccess200CastList();
+      String uri =
+          'https://api.themoviedb.org/3/movie/$id/credits?api_key=$key&language=en-US';
+      // act
+      await dataSource.getCastPeople(id, key);
+      // assert
+      verify(mockHttpClient.get(Uri.parse(uri)));
+    });
+
+    test('should return [CastList] when the status code is 200 (success)',
+        () async {
+      // arrange
+      setUpMockHttopClientSuccess200CastList();
+      // act
+      final result = await dataSource.getCastPeople(id, key);
+      print(result);
+      // assert
+      expect(result[0], equals(tMapCastPeople));
+    });
+
+    test(
+        '''should throw a ServerException when the 
+        response code is 404 or other than 200''',
+        () async {
+      // arrange
+      setUpMockHttopClientFailure404orOtherCastList();
+      // act
+      final call = dataSource.getCastPeople;
       // assert
       expect(call(0, key), throwsA(TypeMatcher<ServerException>()));
     });
